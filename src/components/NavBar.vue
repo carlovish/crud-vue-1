2d266e9960dd5a0826045810ff7c367b1e53f990
carlovish <carloshechava@gmail.com>
2022-07-05 16:12:39 +0100

    Actualizaciones

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,22 @@
 <template>
-    <ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <ul>
         <li>
-            <router-link to="/tasks">Tasks</router-link>
+          <router-link to="/tasks">Tasks</router-link>
         </li>
         <li>
-            <router-link to="/tasks/new">Crear Task</router-link>
+          <router-link to="/tasks/new">Crear Task</router-link>
         </li>
-    </ul>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
-
-    name: "NavBar"
-
-})
+  name: "NavBar",
+});
 </script>
